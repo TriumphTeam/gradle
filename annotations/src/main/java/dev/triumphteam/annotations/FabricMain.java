486f0b99d08ce826ad5a0d6d7ece2ec3981aa36c
Matt <xpsykogfx@gmail.com>
2021-06-20 04:58:55 +0100

    Updated annotations

--- annotations/src/main/java/dev/triumphteam/annotations/FabricMain.java
+++ annotations/src/main/java/dev/triumphteam/annotations/FabricMain.java
@@ -1,11 +1,10 @@
-package me.mattstudios.annotations;
+package dev.triumphteam.annotations;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.RUNTIME)
+@Retention(RetentionPolicy.CLASS)
 @Target(ElementType.TYPE)
-public @interface BukkitPlugin {
-}
+public @interface FabricMain {}
