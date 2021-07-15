b0092ed91a169930019372eb875aab908372218d
Matt <xpsykogfx@gmail.com>
2021-07-15 22:44:46 +0100

    Revert previous commit because gradle sucks

--- plugin/src/main/kotlin/dev/triumphteam/helper/PluginHelper.kt
+++
@@ -1,9 +0,0 @@
-package dev.triumphteam.helper
-
-import org.gradle.kotlin.dsl.PluginDependenciesSpecScope
-import org.gradle.kotlin.dsl.version
-import org.gradle.plugin.use.PluginDependencySpec
-
-fun PluginDependenciesSpecScope.shadow(version: String): PluginDependencySpec {
-    return id("com.github.johnrengelman.shadow") version(version)
-}
